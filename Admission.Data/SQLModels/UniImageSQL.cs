029387f695334c812ab7ead4c7c37950237b16cb
Nguyen Phan Phuoc Anh <anhnpp.microsoft@gmail.com>
2021-11-11 20:05:20 +0000

    remove isBanner

--- Admission.Data/SQLModels/UniImageSQL.cs
+++ Admission.Data/SQLModels/UniImageSQL.cs
@@ -12,6 +12,5 @@ namespace Admission.Data.SQLModels
         public string Src { get; set; }
         public string Alt { get; set; }
         public bool? IsLogo { get; set; }
-        public bool? IsBanner { get; set; }
     }
 }
